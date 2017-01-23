7196707adefa655b93e2791e525f608df1abb166
Michaela-Davis <michaela.delight@gmail.com>
2017-01-22 18:23:23 -0800

    add User Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
+// BUSINESS LOGIC
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
-
 }
+
+// USER LOGIC
+$(document).ready(function() {
+  $("form#newContact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#newFirstName").val();
+    var inputtedLastName = $("input#newLastName").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#newFirstName").val("");
+    $("input#newLastName").val("");
+
+  });
+});
